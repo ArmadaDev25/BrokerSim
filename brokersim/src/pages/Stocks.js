04d8e0312a80e36aaac4614e0b5b353a287b9ae5
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-08-31 08:58:28 -0700

    the AvalibleStocks array is now looped through and elements containing information on the stock display properly

--- brokersim/src/pages/Stocks.js
+++ brokersim/src/pages/Stocks.js
@@ -1,11 +1,18 @@
 import { avalibleStocks } from "../data";
 
+
 function Stocks(){
     return(
         <div>
             <h1>Currently Listed Stocks</h1>
             {avalibleStocks.map((avaliblestock,index)=>{
-                
+                const {symbol, value} = avaliblestock
+                return(
+                    <div>
+                        <h2 key={symbol}>{symbol}</h2>
+
+                    </div>
+                )
             })}
         </div>
     )
