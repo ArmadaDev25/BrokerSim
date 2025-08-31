3e266da3c3684272457b955801f02b3ee1630ab0
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-08-31 16:24:51 -0700

    Got the Stock To Properly Update

--- brokersim/src/pages/StockOrder.js
+++ brokersim/src/pages/StockOrder.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom"
-import { avalibleStocks } from "../data";
+import { avalibleStocks, ownedStocks} from "../data";
 import { useEffect, useState } from "react";
 
 function StockOrder(){
@@ -17,6 +17,32 @@ function StockOrder(){
         setStock(currentStock)
     }
     console.log(stock)
+
+    function addStock(input){
+    let stockOwned = false;
+    for (let x = 0; x < ownedStocks.length; x++){
+      // Checks to see if the user already own the stock
+      if (input.symbol = ownedStocks[x].symbol){
+        //console.log("you own this stock");
+        stockOwned = true;
+        ownedStocks[x].owned = ownedStocks[x].owned + 1;
+      }
+    }
+    // If no owned Stock is found, create a new Stock object and add it to the ownedStocks array
+    if (stockOwned == false){
+      // Create a new object to put into the OwnedStocks array
+      const newStock = new Object({
+        symbol: input.symbol,
+        value: input.value,
+        owned: 1
+      })
+      // Push the newly created object to the OwnedStocks Array
+      ownedStocks.push(newStock);
+    }
+    // Debug that shows that the stock has been added to the OwnedStockArray
+    console.log("Owned After")
+    console.log(ownedStocks)
+  }
 
     useEffect(() => {
         getStock()
@@ -44,6 +70,7 @@ function StockOrder(){
             <h2>
                 {stock.symbol}
             </h2>
+            <button onClick={()=>addStock(stock)}>Place Order</button>
         </div>
         )
     }
